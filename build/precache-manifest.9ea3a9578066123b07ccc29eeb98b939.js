6805dccb472dab76726153df33ea5280b89731fa
alsoderg90 <64720503+alsoderg90@users.noreply.github.com>
2022-07-07 19:27:59 +0300

    style fix (#2)

--- build/precache-manifest.9ea3a9578066123b07ccc29eeb98b939.js
+++ build/precache-manifest.9ea3a9578066123b07ccc29eeb98b939.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9308a615b6d04889867c0312a904707",
+    "revision": "2f103e372852ae23f38400e47cc2c151",
     "url": "/index.html"
   },
   {
-    "revision": "9fe09b7eab2c1c71093c",
+    "revision": "ea91ff4fb6b6ba8b769f",
     "url": "/static/css/2.c1c45ef1.chunk.css"
   },
   {
-    "revision": "2c1946976318130447ff",
+    "revision": "211ee80eb3abc5165a83",
     "url": "/static/css/main.4423fada.chunk.css"
   },
   {
-    "revision": "9fe09b7eab2c1c71093c",
-    "url": "/static/js/2.e798d529.chunk.js"
+    "revision": "ea91ff4fb6b6ba8b769f",
+    "url": "/static/js/2.78d347b1.chunk.js"
   },
   {
     "revision": "ae6bb62977b25ec73f0509e3cbccba19",
-    "url": "/static/js/2.e798d529.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.78d347b1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2c1946976318130447ff",
-    "url": "/static/js/main.6f33bd96.chunk.js"
+    "revision": "211ee80eb3abc5165a83",
+    "url": "/static/js/main.8addfb71.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/static/js/main.6f33bd96.chunk.js.LICENSE.txt"
+    "url": "/static/js/main.8addfb71.chunk.js.LICENSE.txt"
   },
   {
     "revision": "597d348669253ad66271",
